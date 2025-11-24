c447c56480bc896ef92abf0d9056f4c0a8dee414
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-11-24 14:21:21 +0500

    Add project files.

---
+++ Tyuiu.MarakovAD.Sprint5.Task0.V26.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+namespace Tyuiu.MarakovAD.Sprint5.Task0.V26.Lib
+{
+    public class DataService : ISprint5Task0V26
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double res = Math.Round(0.7 * Math.Pow(x,3) + 1.52 * Math.Pow(x,2),3);
+            File.WriteAllText(path, res.ToString());
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MarakovAD.Sprint5.Task0.V26/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MarakovAD.Sprint5.Task0.V26.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Мараков А. Д. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы c классом File                              *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("* Выполнил: Мараков Андрей Дмитриевич | ИБКСб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дано выражение, вычислить его значение при x = 2, " +
+            "результат сохранить в текстовый файл OutPutFileTask0.txt и вывести на консоль.");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        int x = 2;
+        Console.WriteLine("X = " + x);
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
