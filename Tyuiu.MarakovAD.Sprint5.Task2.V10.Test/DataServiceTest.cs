bc65a4cf2496a389b24212263d1aac34020ac584
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-11-24 16:20:20 +0500

    Task2

---
+++ Tyuiu.MarakovAD.Sprint5.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MarakovAD.Sprint5.Task2.V10.Lib;
+namespace Tyuiu.MarakovAD.Sprint5.Task2.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[3, 3] { { 4, 3, -3 }, { -5, -6, -3 }, { -7, -9, -9 } };
+            string path = ds.SaveToFileTextData(matrix);
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.MarakovAD.Sprint5.Task2.V10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using Tyuiu.MarakovAD.Sprint5.Task2.V10.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Мараков А. Д. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы c классом File                              *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнил: Мараков Андрей Дмитриевич | ИБКСб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, " +
+            "заполненный значениями с клавиатуры. Заменить положительные элементы массива на 1, " +
+            "отрицательные на 0. Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        int[,] matrix = new int[3, 3] { { 4, 3, -3 }, { -5, -6, -3 }, { -7, -9, -9 } };
+        int rows = matrix.GetUpperBound(0) + 1;
+        int columns = matrix.Length / rows;
+        Console.WriteLine("Массив: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{matrix[i, j]} \t");
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(matrix);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
